619bdfa1df41a935c9094eafe0a3c9b10837cea4
Jacob Monroe <jm217@uark.edu>
2023-09-05 16:57:48 -0500

    Refined unconditional scripts.

--- scripts/run_uncond_input_creation.sh
+++ scripts/run_uncond_input_creation.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-conda activate tf_protein_env
+# conda activate tf_protein_env
 
-datadir="~/Sidechain_Decoding/clean_pdbs"
-savedir="~/Sidechain_Decoding/training_inputs"
+datadir="${HOME}/Sidechain_Decoding/clean_pdbs"
+savedir="${HOME}/Sidechain_Decoding/training_inputs"
 
 # Training special case residues - GLY, N-terminal hydrogens, and NPRO
 # All will be trained without conditioning on local environment
 
 # GLY
-python -m scdecode.unconditional gen_input ${datadir}/residue_modifications.json GLY -r ${datadir} -s ${savedir}/GLY > gen_inputs_GLY.out
+python -m scdecode.unconditional gen_input ${datadir}/residue_modifications.json GLY -r ${datadir} -s ${savedir}/GLY > gen_inputs_GLY.out 2>&1
 
 # N-terminal
-python -m scdecode.unconditional gen_input ${datadir}/residue_modifications.json Nterm -r ${datadir} -s ${savedir}/Nterm --n_terminal > gen_inputs_Nterm.out
+python -m scdecode.unconditional gen_input ${datadir}/residue_modifications.json Nterm -r ${datadir} -s ${savedir}/Nterm --n_terminal > gen_inputs_Nterm.out 2>&1
 
 # NPRO, an N-terminal special case (won't decode sidechain, just 2 hydrogens instead of 3)
-python -m scdecode.unconditional gen_input ${datadir}/residue_modifications.json NPRO -r ${datadir} -s ${savedir}/NPRO --bat_atom_str @N,CA,C,H2,H3 > gen_inputs_NPRO.out
+python -m scdecode.unconditional gen_input ${datadir}/residue_modifications.json NPRO -r ${datadir} -s ${savedir}/NPRO --bat_atom_str @N,CA,C,H2,H3 > gen_inputs_NPRO.out 2>&1
--- scripts/run_uncond_training.sh
+++ scripts/run_uncond_training.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-conda activate tf_protein_env
+# conda activate tf_protein_env
 
-datadir="~/Sidechain_Decoding/training_inputs"
-savedir="~/Sidechain_Decoding/training_input"
+datadir="${HOME}/Sidechain_Decoding/training_inputs"
 
 # Training special case residues - GLY, N-terminal hydrogens, and NPRO
 # All will be trained without conditioning on local environment
 
 # GLY
-python -m scdecode.unconditional train GLY -r ${datadir}/GLY -s ./ > train_GLY.out
+python -m scdecode.unconditional train GLY -r ${datadir}/GLY -s ./ > train_GLY.out 2>&1
 
 # N-terminal
-python -m scdecode.unconditional train Nterm -r ${datadir}/Nterm -s ./ > train_Nterm.out
+python -m scdecode.unconditional train Nterm -r ${datadir}/Nterm -s ./ > train_Nterm.out 2>&1
 
 # NPRO, an N-terminal special case (won't decode sidechain, just 2 hydrogens instead of 3)
-python -m scdecode.unconditional train NPRO -r ${datadir}/NPRO -s ./ > train_NPRO.out
+python -m scdecode.unconditional train NPRO -r ${datadir}/NPRO -s ./ > train_NPRO.out 2>&1
