13529178f5136ccc875c6ad8c4c65fe00de3b491
Jacob Monroe <jm217@uark.edu>
2023-09-08 12:41:35 -0500

    Added model analysis scripts

---
+++ scripts/run_analysis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=analysis
+#SBATCH --output=%x_%a.out
+#SBATCH --partition gpu72
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=72:00:00
+#SBATCH --array=0-25
+
+echo "Starting time is $(date)"
+
+cd $SLURM_SUBMIT_DIR
+
+module purge
+module load gcc/11.2.1
+module load mkl/19.0.5
+module load python/3.12-anaconda
+conda activate new_tf_protein_env
+
+datadir="/storage/jm217/data_Sidechain_Decoding/training_inputs"
+modeldir="${HOME}/Sidechain_Decoding/trained_models"
+resnames=("ALA" "ARG" "ASH" "ASN" "ASP" "CYM" "CYS" "GLH" "GLN" "GLU" "HID" "HIE" "HIP" "HYP" "ILE" "LEU" "LYN" "LYS" "MET" "PHE" "PRO" "SER" "THR" "TRP" "TYR" "VAL")
+
+export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib:$LD_LIBRARY_PATH
+
+echo "Analyzing sidechain ${resnames[$SLURM_ARRAY_TASK_ID]} decoding"
+
+python -m scdecode.analysis_tools analyze_model "${resnames[$SLURM_ARRAY_TASK_ID]}" -r "${datadir}/${resnames[$SLURM_ARRAY_TASK_ID]}" -m "${modeldir}/${resnames[$SLURM_ARRAY_TASK_ID]}"
+
+echo "Ended at time $(date)"
---
+++ scripts/run_uncond_analysis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# conda activate tf_protein_env
+
+datadir="${HOME}/Sidechain_Decoding/training_inputs"
+modeldir="${HOME}/Sidechain_Decoding/trained_models"
+
+# Analyzing special case residues - GLY, N-terminal hydrogens, and NPRO
+
+# GLY
+python -m scdecode.analysis_tools analyze_model GLY -r ${datadir}/GLY -m ${modeldir}/GLY_decoder/GLY_weights.ckpt --unconditional > analysis_GLY.out 2>&1
+
+# N-terminal
+python -m scdecode.analysis_tools analyze_model Nterm -r ${datadir}/Nterm -m ${modeldir}/Nterm_decoder/Nterm_weights.ckpt --unconditional > analysis_Nterm.out 2>&1
+
+# NPRO, an N-terminal special case (won't decode sidechain, just 2 hydrogens instead of 3)
+python -m scdecode.analysis_tools analyze_model NPRO -r ${datadir}/NPRO -m ${modeldir}/NPRO_decoder/NPRO_weights.ckpt --unconditional > analysis_NPRO.out 2>&1
