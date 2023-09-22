2f4b8cb45d1eaa521d4052ec3026919446881795
jm217 <jm217@uark.edu>
2023-09-22 14:27:27 -0500

    Updated analysis script.

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition agpu72
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=64
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #SBATCH --array=0-25
 
 echo "Starting time is $(date)"
@@ -17,14 +17,14 @@ module load mkl/19.0.5
 module load python/3.12-anaconda
 conda activate new_tf_protein_env
 
-datadir="/storage/jm217/data_Sidechain_Decoding/training_inputs"
-modeldir="${HOME}/Sidechain_Decoding/trained_models"
+datadir="/storage/jm217/data_Sidechain_Decoding/energy_min_training_inputs"
+modeldir="${HOME}/Sidechain_Decoding/energy_min_trained_models"
 resnames=("ALA" "ARG" "ASH" "ASN" "ASP" "CYM" "CYS" "GLH" "GLN" "GLU" "HID" "HIE" "HIP" "HYP" "ILE" "LEU" "LYN" "LYS" "MET" "PHE" "PRO" "SER" "THR" "TRP" "TYR" "VAL")
 
 export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib:$LD_LIBRARY_PATH
 
 echo "Analyzing sidechain ${resnames[$SLURM_ARRAY_TASK_ID]} decoding"
 
-python -m scdecode.analysis_tools analyze_model "${resnames[$SLURM_ARRAY_TASK_ID]}" -r "${datadir}/${resnames[$SLURM_ARRAY_TASK_ID]}" -m "${modeldir}/${resnames[$SLURM_ARRAY_TASK_ID]}"
+python -m scdecode.analysis_tools analyze_model "${resnames[$SLURM_ARRAY_TASK_ID]}" -r "${datadir}/${resnames[$SLURM_ARRAY_TASK_ID]}" -m "${modeldir}/${resnames[$SLURM_ARRAY_TASK_ID]}_decoder/${resnames[$SLURM_ARRAY_TASK_ID]}_weights.ckpt"
 
 echo "Ended at time $(date)"
