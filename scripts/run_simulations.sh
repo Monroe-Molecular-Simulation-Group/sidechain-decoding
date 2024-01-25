1f1d5c60a34c634522e981717c0f9acf45d8b0c4
Jacob Monroe <jacob.monroe@uark.edu>
2024-01-25 15:33:27 -0600

    Added slurm script for running simulations.

---
+++ scripts/run_simulations.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=protein_sims
+#SBATCH --output=%x.out
+#SBATCH --partition agpu72
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --time=24:00:00
+
+echo "Starting time is $(date)"
+
+cd $SLURM_SUBMIT_DIR
+
+module purge
+module load gcc/11.2.1
+module load mkl/19.0.5
+module load nvhpc/22.7
+module load python/3.12-anaconda
+conda activate new_tf_protein_env
+
+pdbfile="/storage/jm217/data_Sidechain_Decoding/simulations/1UAO.pdb"
+
+export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib:$LD_LIBRARY_PATH
+
+python ~/bin/sidechain-decoding/simulations/openmm_sims.py $pdbfile -n 500000000 --restrain --tempering
+
+echo "Ended at time $(date)"
