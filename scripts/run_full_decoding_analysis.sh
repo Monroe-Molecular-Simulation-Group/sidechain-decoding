74578f6051232c394543c47d99a487ac58e877a3
Jacob Monroe <jacob.monroe@uark.edu>
2024-01-26 20:29:24 -0600

    Preliminarily added script for running full decoding analysis on cluster.

---
+++ scripts/run_full_decoding_analysis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=full_decode
+#SBATCH --output=%x.out
+#SBATCH --partition agpu72
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --time=24:00:00
+
+echo "Starting time is $(date)"
+
+cd $SLURM_SUBMIT_DIR
+
+module purge
+module load gcc/11.2.1
+module load mkl/19.0.5
+module load nvhpc/22.7
+module load python/3.12-anaconda
+conda activate new_tf_protein_env
+
+export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib:$LD_LIBRARY_PATH
+
+python -m scdecode.full_protein_decoding ~/Sidechain_Decoding/simulations/1UAO.pdb ~/Sidechain_Decoding/simulations/openmm_restrained/1UAO_restrained.nc -b ~/Sidechain_Decoding/energy_min_training_inputs -m ~/Sidechain_Decoding/logprob_trained_models
+
+echo "Ended at time $(date)"
