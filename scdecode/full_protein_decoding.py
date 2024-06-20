e0552fcafcbe8ea77341127753b1ff287b61ae8d
Jacob Monroe <jacob.monroe@uark.edu>
2024-06-20 09:45:17 -0500

    Updated sampling for individual residues to avoid overflows.

--- scdecode/full_protein_decoding.py
+++ scdecode/full_protein_decoding.py
@@ -756,16 +756,34 @@ def analyze_individual_residues(pdb_file, bat_dir='./', model_dir='./', out_name
         # Generate predicted probability distribution for BAT coordinates
         this_dist = model_dict[res.name](this_model_inputs)
 
-        # Sample n_samples times from it
-        sample = tf.squeeze(this_dist.sample(n_samples))
-        logprobs = this_dist.log_prob(sample)
+        # Sample n_samples times from it, in batches to avoid overflows
+        full_bat_sample = []
+        logprobs = []
+        n_sub_sample = 10000
+        tot_sampled = 0
+        while tot_sampled < n_samples:
+            if (n_samples - tot_sampled) >= n_sub_sample:
+                this_n_sample = n_sub_sample
+            else:
+                this_n_sample = (n_samples % n_sub_sample)
 
-        # Fill in hydrogens
-        full_bat_sample = coord_transforms.fill_in_h_bonds_tf(sample, *h_info_dict[res.name])
+            tot_sampled += this_n_sample
 
-        # Convert BAT coordinates to xyz
-        full_bat_sample = coord_transforms.fill_in_bat(full_bat_sample,
-                            np.tile(this_root_coords, (n_samples, 1, 1)).astype('float32'))
+            sample = tf.squeeze(this_dist.sample(this_n_sample))
+            logprobs.append(this_dist.log_prob(sample))
+
+            # Fill in hydrogens
+            this_bat_sample = coord_transforms.fill_in_h_bonds_tf(sample, *h_info_dict[res.name])
+
+            # Convert BAT coordinates to xyz
+            this_bat_sample = coord_transforms.fill_in_bat(this_bat_sample,
+                                   np.tile(this_root_coords, (n_samples, 1, 1)).astype('float32'))
+
+            full_bat_sample.append(this_bat_sample)
+
+        full_bat_sample = tf.concat(full_bat_sample, axis=0)
+        logprobs = tf.concat(logprobs, axis=0)
+        
         sample_xyz = coord_transforms.bat_cartesian_tf(full_bat_sample, bat_dict[res.name])
 
         # Save sample BAT coordinates, including root positions
